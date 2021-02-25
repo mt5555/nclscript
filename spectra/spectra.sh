a642c6eed3f518a908d6e6d39960e805e1d3d99a
Mark Taylor <mt@mp3dev.org>
2021-02-25 13:09:32 -0800

    spectra script

---
+++ spectra/spectra.sh
@@ -0,0 +1,24 @@
+#!/bin/tcsh
+#
+#  regular haswell node: 128GB
+#  shared haswell node: max 118GB
+#  bigmem: 750GB max
+#
+#SBATCH --job-name ncremap
+#SBATCH -N 1
+#SBATCH --time=48:00:00
+#SBATCH -C haswell
+#XXSBATCH -n 1 --mem 118GB -q shared
+#SBATCH -q regular
+#XXSBATCH --clusters=escori
+#XXSBATCH --qos=bigmem
+#XXSBATCH --mem=128GB          
+
+date
+cd ~/scratch2/dyamond/dy2
+
+#ncl ~/codes/nclscript/spectra/ke.ncl   't1=22.000' 't2=24.000' 'tinc=0.125'  
+unbuffer ncl ~/codes/nclscript/spectra/ke.ncl  'plvl=500'  't1=22.000' 't2=24.000' 'tinc=0.125'  
+
+
+date
