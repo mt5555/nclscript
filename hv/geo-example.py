e6165c4d91897001af1c4de35c07e5905bc88e2c
Mark Taylor <mt@mp3dev.org>
2024-05-24 15:50:23 -0600

    geoviews example working

--- hv/geo-example.py
+++ hv/geo-example.py
@@ -1,45 +1,39 @@
 import holoviews as hv
 import geoviews as gv
 import geoviews.feature as gf
-import matplotlib.pyplot as plt
-from bokeh.plotting import show
-
-import matplotlib
-#matplotlib.use('Qt5Agg')
- 
-from geoviews import opts
 from cartopy import crs
-
-
-#gv.extension('bokeh', 'matplotlib')
+#
+# modified example from:
+# https://geoviews.org/user_guide/Projections.html#
+#
 gv.extension('matplotlib')
+
+#projplot = crs.Orthographic(central_longitude=-90, central_latitude=30)
+projplot = crs.Robinson()
+#projplot = crs.PlateCarree())
 
 
 nyc, beijing = (-74.0, 40.7, 'NYC'), (116.4, 39.9, 'Beijing')
 london = (14471.53, 6712008., 'London')
 
 cities_lonlat   = gv.Points([nyc, beijing], vdims='City')
+cities_lonlat.opts(color='red')
 cities_mercator = gv.Points([london], crs=crs.GOOGLE_MERCATOR, vdims='City')
+# convert mercator to lonlat:
+cities_lonlat2 = gv.operation.project(cities_mercator, projection=crs.PlateCarree())
+cities_lonlat2.opts(color='black')
 
-#(gv.tile_sources.OSM * cities_lonlat * cities_mercator).opts(
-#    opts.Points(global_extent=True, width=500, height=475, size=12, color='black'))
 
-
-#plot = gv.tile_sources.OSM * cities_lonlat * cities_mercator
-#plot.opts(  opts.Points(global_extent=True, width=500, height=475, size=12, color='black'))
-
-#features = gv.Overlay([gf.ocean, gf.land, gf.rivers, gf.lakes, gf.borders, gf.coastline])
-#gv.output(features, backend='matplotlib', fig='png', size=300)
-
-r = gv.Overlay([gf.coastline]) * cities_lonlat 
+r = gv.Overlay([gf.coastline,gf.ocean,gf.land]) * cities_lonlat * cities_lonlat2
+#r.opts(  gv.opts.Points(color='black'))
 r.opts(xlim=(-180.,180))
 r.opts(ylim=(-90.,90))
 r.opts(data_aspect=1)
-#r.opts(frame_width=100)
+r.opts(projection=projplot, global_extent=True)
+r.opts(fig_inches=8)
 
-rplot=gv.render(r,backend='matplotlib')
-#rplot.savefig("geo-example.png", bbox_inches='tight',dpi=300)
-
+rplot=gv.render(r)
+rplot.savefig("geo-example.png", bbox_inches='tight',dpi=600)
 rplot.show()
 input("pausing to disply plot. hit any key")
 
